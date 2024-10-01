3d55fb12777a6fc53bc70bf953f5d2e2cb5746f6
Akshita kumari <110122283+akshita317@users.noreply.github.com>
2024-10-01 23:23:31 +0530

    Create linear.py

---
+++ linear.py
@@ -0,0 +1,15 @@
+# Python program to check fixed point 
+# in an array using linear search
+def linearSearch(arr, n):
+    for i in range(n):
+        if arr[i] is i:
+            return i
+    # If no fixed point present then return -1
+    return -1
+
+# Driver program to check above functions
+arr = [-10, -1, 0, 3, 10, 11, 30, 50, 100]
+n = len(arr)
+print("Fixed Point is " + str(linearSearch(arr, n)))
+
+# This code is contributed by Pratik Chhajer
